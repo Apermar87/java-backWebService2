388a57ec7461ab9eb40d9111161dd6eef5f0b94b
Apermar87 <ajperezmarin@gmail.com>
2024-10-04 17:54:15 +0200

    init commit

---
+++ src/main/java/com/example/webserviceEjercicio/Productos.java
@@ -0,0 +1,64 @@
+package com.example.webserviceEjercicio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Productos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+
+    public Productos(){
+        nombre = "";
+        descripcion = "";
+        precio = 0.0;
+        stock = 0;
+    }
+
+    public Productos(String nombre, String descripcion, double precio, int stock) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
---
+++ src/main/java/com/example/webserviceEjercicio/WebserviceEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.webserviceEjercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceEjercicioApplication.class, args);
+	}
+
+}
