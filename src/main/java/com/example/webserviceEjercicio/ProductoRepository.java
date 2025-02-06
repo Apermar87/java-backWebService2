bb95dea4f598d4b441b058d0d1b7bb541c76a5f3
Apermar87 <ajperezmarin@gmail.com>
2025-02-06 11:01:31 +0100

    Eliminar clases duplicadas

--- src/main/java/com/example/webserviceEjercicio/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webserviceEjercicio;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductoRepository extends MongoRepository<Productos, String> {
-}
-
